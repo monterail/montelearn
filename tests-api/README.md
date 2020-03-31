f51c2a8db5cfd3b9234440d70b416fc85563e2e3
Aitor Lopez Beltran <aitorlopezbeltran@gmail.com>
2020-03-31 17:46:05 +0200

    Add dip configuration file

--- tests-api/README.md
+++ tests-api/README.md
@@ -4,4 +4,12 @@
 
 It's based on https://evilmartians.com/chronicles/ruby-on-whales-docker-for-ruby-rails-development
 You need only docker to develop the application.
-All the shell commands pls run inside `docker-compose run --rm runner`
+All the shell commands pls run inside `docker-compose run --rm runner` or `dip bash`
+
+To improve docker interaction on development we recommend to use dip
+https://github.com/bibendi/dip#dip
+
+```
+# Run commands each by each from provision section of dip.yml
+dip provision
+```
---
+++ tests-api/db/structure.sql
@@ -0,0 +1,143 @@
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: pgcrypto; Type: EXTENSION; Schema: -; Owner: -
+--
+
+CREATE EXTENSION IF NOT EXISTS pgcrypto WITH SCHEMA public;
+
+
+--
+-- Name: EXTENSION pgcrypto; Type: COMMENT; Schema: -; Owner: -
+--
+
+COMMENT ON EXTENSION pgcrypto IS 'cryptographic functions';
+
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: ar_internal_metadata; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.ar_internal_metadata (
+    key character varying NOT NULL,
+    value character varying,
+    created_at timestamp(6) without time zone NOT NULL,
+    updated_at timestamp(6) without time zone NOT NULL
+);
+
+
+--
+-- Name: questions; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.questions (
+    id uuid DEFAULT public.gen_random_uuid() NOT NULL,
+    test_id uuid,
+    type character varying NOT NULL,
+    content text NOT NULL,
+    options jsonb NOT NULL,
+    created_at timestamp(6) without time zone NOT NULL,
+    updated_at timestamp(6) without time zone NOT NULL
+);
+
+
+--
+-- Name: schema_migrations; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.schema_migrations (
+    version character varying NOT NULL
+);
+
+
+--
+-- Name: tests; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.tests (
+    id uuid DEFAULT public.gen_random_uuid() NOT NULL,
+    title character varying NOT NULL,
+    created_at timestamp(6) without time zone NOT NULL,
+    updated_at timestamp(6) without time zone NOT NULL
+);
+
+
+--
+-- Name: ar_internal_metadata ar_internal_metadata_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.ar_internal_metadata
+    ADD CONSTRAINT ar_internal_metadata_pkey PRIMARY KEY (key);
+
+
+--
+-- Name: questions questions_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.questions
+    ADD CONSTRAINT questions_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: schema_migrations schema_migrations_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.schema_migrations
+    ADD CONSTRAINT schema_migrations_pkey PRIMARY KEY (version);
+
+
+--
+-- Name: tests tests_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.tests
+    ADD CONSTRAINT tests_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: index_questions_on_test_id; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX index_questions_on_test_id ON public.questions USING btree (test_id);
+
+
+--
+-- Name: index_questions_on_type; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX index_questions_on_type ON public.questions USING btree (type);
+
+
+--
+-- Name: questions fk_rails_cb4b358d50; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.questions
+    ADD CONSTRAINT fk_rails_cb4b358d50 FOREIGN KEY (test_id) REFERENCES public.tests(id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+SET search_path TO "$user", public;
+
+INSERT INTO "schema_migrations" (version) VALUES
+('20200401185622'),
+('20200402151557'),
+('20200406195624');
+
+
