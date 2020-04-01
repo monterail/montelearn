5eb4ee22e1a82115cae0f4811a1f5ba0823c4cad
Ewelina Buturla <ewelina.buturla@monterail.com>
2020-04-01 13:08:12 +0200

    Code refactor

--- lesson-api/api/lesson/admin.py
+++ lesson-api/api/lesson/admin.py
@@ -3,12 +3,4 @@ from django.contrib import admin
 from .models import Lesson
 
 
-class LessonAdmin(admin.ModelAdmin):
-    fields = ("name", "description", "pdf_file")
-    readonly_fields = ("uuid",)
-
-    class Meta:
-        model = Lesson
-
-
 admin.site.register(Lesson)
