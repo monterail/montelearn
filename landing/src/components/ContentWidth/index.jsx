47b9402bcda0fede9f069bc2697b651cc6d8cb2b
Jędrzej Ginter <jedrzej.ginter@monterail.com>
2020-04-02 09:50:24 +0200

    [ME-9] ESLint + Prettier (#9)

--- landing/src/components/ContentWidth/index.jsx
+++ landing/src/components/ContentWidth/index.jsx
@@ -1,5 +1,16 @@
-import { rem } from "../../theming/utils";
+import { rem } from "@/theming/utils";
 
-export default function ContentWidth({ as: C = 'div', style = {}, ...props }) {
-  return <C style={{ marginLeft: 'auto', marginRight: 'auto', maxWidth: rem(1024), width: '100%', ...style }} {...props} />
+export default function ContentWidth({ as: C = "div", style = {}, ...props }) {
+  return (
+    <C
+      style={{
+        marginLeft: "auto",
+        marginRight: "auto",
+        maxWidth: rem(1024),
+        width: "100%",
+        ...style,
+      }}
+      {...props}
+    />
+  );
 }
