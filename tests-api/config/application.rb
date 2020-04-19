df89ec18a6ee606a88037a3b22e705ca4b94faa8
Aitor Lopez Beltran <aitorlopezbeltran@gmail.com>
2020-04-19 10:52:45 +0200

    Update app config

--- tests-api/config/application.rb
+++ tests-api/config/application.rb
@@ -2,15 +2,15 @@ require_relative 'boot'
 
 require "rails"
 # Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
+# require "active_model/railtie"
+# require "active_job/railtie"
 require "active_record/railtie"
 # require "active_storage/engine"
 require "action_controller/railtie"
 # require "action_mailer/railtie"
 # require "action_mailbox/engine"
 # require "action_text/engine"
-require "action_view/railtie"
+# require "action_view/railtie"
 # require "action_cable/engine"
 # require "sprockets/railtie"
 # require "rails/test_unit/railtie"
@@ -35,5 +35,6 @@ module TestsApi
     config.api_only = true
 
     config.active_record.schema_format = :sql
+    config.generators.system_tests = nil
   end
 end
