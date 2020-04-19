676754d19e3ee8f4790b9373b298d7eb09decc5c
Aitor Lopez Beltran <aitorlopezbeltran@gmail.com>
2020-04-19 16:33:53 +0200

    Update routes

--- tests-api/config/routes.rb
+++ tests-api/config/routes.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  scope :api do
+    resources :tests, only: %i(index show)
+  end
 end
