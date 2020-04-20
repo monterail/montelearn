171e6b749e16367d55b4ea6871874de7b3bd5826
Aitor Lopez Beltran <aitorlopezbeltran@gmail.com>
2020-04-20 19:43:44 +0200

    Explore GET /lesson/{uuid}/ with ShowGuesser
    - Trying to circumvent that react-admin uses record.id

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ListGuesser } from 'react-admin';
+import { ListGuesser, ShowGuesser } from 'react-admin';
 import { Admin, Resource } from 'react-admin';
 import dataProvider from './dataProvider';
 import authProvider from './authProvider';
@@ -7,7 +7,7 @@ import { LessonList } from './lessons';
 
 const App = () => (
   <Admin dataProvider={dataProvider} authProvider={authProvider} >
-    <Resource name="lesson" list={LessonList} />
+    <Resource name="lesson" list={LessonList} show={ShowGuesser} />
   </Admin>
 )
 
--- admin/src/lessons.js
+++ admin/src/lessons.js
@@ -3,13 +3,19 @@ import {
   List,
   Datagrid,
   TextField,
+  ShowButton,
 } from 'react-admin';
+
+const ListActions = ({ record, basePath, resource }) => (
+  <ShowButton basePath={basePath} record={{ id: record["uuid"] }} />
+);
 
 export const LessonList = props => (
   <List {...props} >
     <Datagrid>
       <TextField source="name" />
       <TextField source="description" />
+      <ListActions />
     </Datagrid>
   </List>
 );
