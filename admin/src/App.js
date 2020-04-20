3d5eda903641d80948002157d7991f80156753cd
Aitor Lopez Beltran <aitorlopezbeltran@gmail.com>
2020-04-20 19:32:52 +0200

    Explore GET /lesson​/ with ListGuesser

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
-import { Admin } from 'react-admin';
+import { ListGuesser } from 'react-admin';
+import { Admin, Resource } from 'react-admin';
 import dataProvider from './dataProvider';
 
-const App = () => <Admin dataProvider={dataProvider} />;
+const App = () => (
+  <Admin dataProvider={dataProvider} >
+    <Resource name="lesson" list={ListGuesser} />
+  </Admin>
+)
 
 export default App;
