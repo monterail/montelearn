e6accbaa77a8aa939eda9abb5de5b5d47ecfb76d
Jedrzej Ginter <jedrzej.ginter@monterail.com>
2020-04-21 14:24:31 +0200

    Make form simpler

--- landing/src/components/TryItOut/index.jsx
+++ landing/src/components/TryItOut/index.jsx
@@ -1,51 +1,25 @@
+import fetch from "isomorphic-unfetch";
 import Head from "next/head";
 import { useCallback, useState } from "react";
 
-import Button from "@/components/Button";
-import { FONT_MONOSPACE, COLOR_GREEN } from "@/theming/const";
+import { FONT_MONOSPACE, COLOR_GREEN, COLOR_WHITE, FONT_SANS_SERIF } from "@/theming/const";
 import { rem } from "@/theming/utils";
 import { ensureLeadingSlash } from "@/utils/paths";
 
 import RecursiveReveal, { WithContext } from "./RecursiveReveal";
 
+const predefinedAPIs = {
+  lessonsApi: { method: "GET", url: "/api/lesson" },
+};
+
 export default function TryItOutComponent() {
-  const [prettify, setPrettify] = useState(true);
-  const [params, setParams] = useState(JSON.stringify({ test: 123 }, null, 2));
-  const [url, setUrl] = useState("/api/lesson");
-  const [method, setMethod] = useState("GET");
+  const [params, setParams] = useState(JSON.stringify({ id: 1 }, null, 2));
   const [request, setRequest] = useState({});
-  const [output, setOutput] = useState("cooked");
+  const [apiConfig, setApiConfig] = useState("lessonsApi");
 
-  const onPrettifyChange = useCallback(() => {
-    const newPrettify = !prettify;
-    setPrettify(newPrettify);
-
-    if (newPrettify) {
-      try {
-        const prettifiedParams = JSON.stringify(JSON.parse(params), null, 2);
-
-        // Avoid unnecessary re-render.
-        if (prettifiedParams === params) {
-          return;
-        }
-
-        setParams(prettifiedParams);
-      } catch {
-        // Ignore.
-      }
-    }
-  }, [prettify, params]);
-
-  const onUrlChange = useCallback((evt) => {
-    setUrl(evt.target.value);
-  }, []);
-
-  const onOutputFormatChange = useCallback((evt) => {
-    setOutput(evt.target.value);
-  }, []);
-
-  const onMethodChange = useCallback((evt) => {
-    setMethod(evt.target.value);
+  const onChangeApi = useCallback((evt) => {
+    setApiConfig(evt.target.value);
+    setRequest({});
   }, []);
 
   const onParamsChange = useCallback((evt) => {
@@ -57,28 +31,33 @@ export default function TryItOutComponent() {
       evt.preventDefault();
 
       try {
+        const { url, method } = predefinedAPIs[apiConfig];
         const parsedParams = JSON.parse(params);
 
-        if (prettify) {
-          setParams(JSON.stringify(parsedParams, null, 2));
-        }
+        setParams(JSON.stringify(parsedParams, null, 2));
 
-        try {
-          const res = await fetch(`http://localhost:8000${ensureLeadingSlash(url)}`);
-          setRequest({
-            params: parsedParams,
-            url,
-            method,
-            response: await res.json(),
-          });
-        } catch {
-          // TODO
-        }
+        const res = await fetch(`http://localhost:8000${ensureLeadingSlash(url)}`, {
+          method,
+          data: params,
+        });
+
+        setRequest({
+          params: parsedParams,
+          url,
+          method,
+          response: {
+            body: res.ok ? (await res.json()).data : null,
+            headers: Array.from(res.headers.entries()),
+            ok: res.ok,
+            status: res.status,
+            statusText: res.statusText,
+          },
+        });
       } catch {
         // Ignore error.
       }
     },
-    [prettify, params, url, method],
+    [params, apiConfig],
   );
 
   const lines = (params.match(/\n/g) || []).length + 1;
@@ -94,54 +73,66 @@ export default function TryItOutComponent() {
       <form css={{ display: "flex", flexDirection: "column" }} onSubmit={onSend}>
         <div
           css={{
-            border: "none",
+            alignItems: "stretch",
+            backgroundColor: COLOR_WHITE,
+            border: `1px solid ${COLOR_GREEN}`,
+            borderBottom: "none",
+            borderRadius: rem(4, 4, 0, 0),
             display: "flex",
             justifyContent: "space-between",
-            margin: rem(0, 0, 10),
             padding: 0,
           }}
         >
           <select
-            id="method"
             css={{
               MozAppearance: "none",
               WebkitAppearance: "none",
               appearance: "none",
-              border: `2px solid ${COLOR_GREEN}`,
-              borderRadius: rem(4),
+              backgroundColor: "transparent",
+              border: "none",
               fontFamily: FONT_MONOSPACE,
               fontSize: rem(14),
               outline: "none",
               padding: rem(5, 10),
+              width: `calc(100% - ${rem(100)})`,
+            }}
+            name="apiConfig"
+            value={apiConfig}
+            onChange={onChangeApi}
+          >
+            {Object.keys(predefinedAPIs).map((k) => (
+              <option value={k}>
+                {predefinedAPIs[k].method} {predefinedAPIs[k].url}
+              </option>
+            ))}
+          </select>
+          <button
+            css={{
+              MozAppearance: "none",
+              WebkitAppearance: "none",
+              appearance: "none",
+              backgroundColor: COLOR_GREEN,
+              border: "none",
+              borderRadius: rem(4),
+              color: COLOR_WHITE,
+              cursor: "pointer",
+              fontFamily: FONT_SANS_SERIF,
+              fontSize: rem(14),
+              fontWeight: 700,
+              height: rem(32),
+              margin: rem(5),
+              padding: 0,
               width: rem(80),
             }}
-            name="method"
-            value={method}
-            onChange={onMethodChange}
+            type="submit"
           >
-            <option value="GET">GET</option>
-            <option value="POST">POST</option>
-          </select>
-          <input
-            css={{
-              border: `2px solid ${COLOR_GREEN}`,
-              borderRadius: rem(4),
-              fontFamily: FONT_MONOSPACE,
-              fontSize: rem(14),
-              outline: "none",
-              padding: rem(5, 10),
-              width: `calc(100% - ${rem(90)})`,
-            }}
-            onChange={onUrlChange}
-            name="url"
-            type="text"
-            value={url}
-          />
+            Send
+          </button>
         </div>
         <textarea
           css={{
             border: `1px solid ${COLOR_GREEN}`,
-            borderRadius: rem(4),
+            borderRadius: rem(0, 0, 4, 4),
             fontFamily: FONT_MONOSPACE,
             fontSize: rem(12),
             margin: rem(0, 0, 10),
@@ -151,98 +142,36 @@ export default function TryItOutComponent() {
           name="body"
           data-lines={lines}
           style={{
-            height: rem(Math.min(10, lines) * 16 + 20),
+            height: rem(Math.min(10, lines) * 16 + 25),
           }}
           onChange={onParamsChange}
           value={params}
         />
-        <div css={{ alignItems: "center", display: "flex", justifyContent: "space-between" }}>
-          <label
-            css={{
-              alignItems: "center",
-              border: "none",
-              display: "flex",
-              flexWrap: "nowrap",
-              margin: 0,
-              padding: 0,
-            }}
-            htmlFor="prettify"
-          >
-            <input
-              checked={prettify}
-              css={{ margin: 0 }}
-              id="prettify"
-              name="prettify"
-              onChange={onPrettifyChange}
-              type="checkbox"
-            />
-            <span css={{ marginLeft: rem(10) }}>Prettify params</span>
-          </label>
-          <Button type="submit">Send</Button>
-        </div>
       </form>
       <WithContext>
-        {request.response && (
-          <div
-            css={{
-              border: `2px solid ${COLOR_GREEN}`,
-              borderRadius: rem(4),
-              fontSize: rem(12),
-              marginTop: rem(10),
-              padding: rem(5, 10),
-            }}
-          >
-            <label htmlFor="cooked">
-              <input
-                id="cooked"
-                checked={output === "cooked"}
-                type="radio"
-                name="output"
-                onChange={onOutputFormatChange}
-                value="cooked"
-              />
-              <span css={{ fontSize: rem(14), marginLeft: rem(5) }}>Highlighted</span>
-            </label>
-            <label htmlFor="raw">
-              <input
-                id="raw"
-                checked={output === "raw"}
-                type="radio"
-                name="output"
-                onChange={onOutputFormatChange}
-                value="raw"
-              />
-              <span css={{ fontSize: rem(14), marginLeft: rem(5) }}>Raw</span>
-            </label>
-          </div>
-        )}
-        {request.response ? (
-          <div
-            css={{
-              fontFamily: FONT_MONOSPACE,
-              border: `1px solid ${COLOR_GREEN}`,
-              padding: rem(5, 10),
-              marginTop: rem(5),
-            }}
-          >
-            {output === "cooked" ? (
-              <div
-                css={{
-                  fontSize: rem(14),
-                  whiteSpace: "nowrap",
-                  lineHeight: rem(24),
-                  overflow: "auto",
-                }}
-              >
-                <RecursiveReveal id="root" value={request.response.data} />
-              </div>
-            ) : (
-              <pre style={{ fontSize: rem(16), wordBreak: "break-all", whiteSpace: "pre-wrap" }}>
-                {JSON.stringify(request.response.data)}
-              </pre>
-            )}
-          </div>
-        ) : null}
+        {request.response &&
+          (request.response.ok ? (
+            <div
+              css={{
+                backgroundColor: COLOR_WHITE,
+                border: `1px solid ${COLOR_GREEN}`,
+                borderRadius: rem(4),
+                fontFamily: FONT_MONOSPACE,
+                fontSize: rem(14),
+                lineHeight: rem(24),
+                marginTop: rem(10),
+                overflow: "auto",
+                padding: rem(5, 10),
+                whiteSpace: "nowrap",
+              }}
+            >
+              <RecursiveReveal id="root" value={request.response.body} />
+            </div>
+          ) : (
+            <div>
+              {request.response.status} {request.response.statusText}
+            </div>
+          ))}
       </WithContext>
     </>
   );
--- landing/src/theming/const.js
+++ landing/src/theming/const.js
@@ -5,5 +5,5 @@ export const COLOR_MILANO_RED = "#b70505";
 export const COLOR_WHITE = "#fff";
 
 export const FONT_SANS_SERIF = "Roboto, sans-serif";
-export const FONT_SERIF = "Playfair Display, serif";
-export const FONT_MONOSPACE = "Fira Code, monspace";
+export const FONT_SERIF = "'Playfair Display', serif";
+export const FONT_MONOSPACE = "'Fira Code', monspace";
