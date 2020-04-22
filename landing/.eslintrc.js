6330a7c8021a2a97ad0774946b630b6a983ab6bf
Jedrzej Ginter <jedrzej.ginter@monterail.com>
2020-04-22 16:33:20 +0200

    Add typescript

--- landing/.eslintrc.js
+++ landing/.eslintrc.js
@@ -1,45 +1,50 @@
 const path = require("path");
 
 module.exports = {
-  parser: "babel-eslint",
+  parser: "@typescript-eslint/parser",
   parserOptions: {
+    project: "./tsconfig.json",
     ecmaVersion: 2018,
     sourceType: "module",
     ecmaFeatures: {
-      jsx: true
-    }
+      jsx: true,
+    },
   },
+  extends: [
+    "airbnb-typescript",
+    "prettier",
+    "plugin:prettier/recommended",
+    "prettier/react",
+    "prettier/@typescript-eslint",
+  ],
+  plugins: ["@typescript-eslint", "jest"],
   env: {
     browser: true,
     node: true,
-    "jest/globals": true
+    "jest/globals": true,
   },
   settings: {
     react: {
-      version: "detect"
+      version: "detect",
     },
     "import/resolver": {
       alias: {
-        extensions: [".js", ".jsx"],
-        map: [
-          ["@", path.join(__dirname, "src")]
-        ]
-      }
-    }
+        extensions: [".ts", ".tsx", ".js", ".jsx"],
+        map: [["@", path.join(__dirname, "src")]],
+      },
+    },
   },
-  extends: ["airbnb", "airbnb/hooks", "prettier"],
-  plugins: ["jest", "prettier"],
   rules: {
+    "@typescript-eslint/quotes": ["error", "double"],
     "import/prefer-default-export": "off",
     "no-restricted-syntax": "off",
     "prettier/prettier": ["error"],
-    "quotes": ["error", "double"],
     "react/jsx-one-expression-per-line": "off",
     "react/jsx-props-no-spreading": "off",
     "react/no-array-index-key": "off",
     "react/no-danger": "off",
     "react/no-unescaped-entities": "off",
     "react/prop-types": "off",
-    "react/react-in-jsx-scope": "off"
-  }
-}
+    "react/react-in-jsx-scope": "off",
+  },
+};
