bed5d3f627116743e0e69f8150a0a746d3e1350c
Aitor Lopez Beltran <aitorlopezbeltran@gmail.com>
2020-04-23 10:16:26 +0200

    [ME-25] Update DB

--- tests-api/db/migrate/20200419090040_create_tests.rb
+++ tests-api/db/migrate/20200419090040_create_tests.rb
@@ -3,9 +3,11 @@
 class CreateTests < ActiveRecord::Migration[6.0]
   def change
     create_table :tests, id: :uuid do |t|
+      t.uuid :lesson_id
+      t.string :question_type, null: false
       t.string :subject, null: false
-      t.string :question, null: false
-      t.string :answer, null: false
+      t.text :question, null: false
+      t.text :choices, null: false
 
       t.timestamps
     end
