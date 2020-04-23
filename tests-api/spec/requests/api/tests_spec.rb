82ece518fee5ee0b4a4bc1e3f23c94548b42df92
Aitor Lopez Beltran <aitorlopezbeltran@gmail.com>
2020-04-23 10:48:12 +0200

    [ME-25] Update request spec

--- tests-api/spec/requests/api/tests_spec.rb
+++ tests-api/spec/requests/api/tests_spec.rb
@@ -7,54 +7,15 @@ RSpec.describe "api/tests", type: :request do
     get "Retrieves tests" do
       tags "Tests"
       produces "application/json"
-
-      parameter name: :limit,
-                in: :query,
-                type: :integer,
-                description: "Number of results to return per page.",
-                required: false
-      parameter name: :offset,
-                in: :query,
-                type: :integer,
-                description: "The initial index from which to return the results.",
-                required: false
+      parameter name: :limit, in: :query, type: :integer, required: false,
+                description: "Number of results to return per page."
+      parameter name: :offset, in: :query, type: :integer, required: false,
+                description: "The initial index from which to return the results."
 
       response "200", :success do
-        schema type: :object,
-               properties: {
-                 count: { type: :integer },
-                 next: { type: :integer, nullable: true },
-                 previous: { type: :integer, nullable: true },
-                 results: {
-                   type: :array,
-                   items: {
-                     type: :object,
-                     properties: {
-                       uuid: { type: :string },
-                       subject: { type: :string },
-                       question: { type: :string },
-                       answer: { type: :string },
-                     },
-                   },
-                 },
-               },
-               required: %w(count next previous results)
-
-        examples "application/json" => {
-          count: 10,
-          next: nil,
-          previous: nil,
-          results: [
-            {
-              uuid: "b77eb326-efc1-41e1-a37e-c66bcff4f340",
-              subject: "Science",
-              question: "Is math related to science?",
-              answer: "Non't",
-            },
-          ],
-        }
-
-        let(:test) { create(:test, :binary_question) }
+        schema RswagHelper::COLLECTION_SCHEMA
+        examples RswagHelper::COLLECTION_EXAMPLE
+        before { create(:test, :binary_question) }
 
         run_test!
       end
@@ -65,43 +26,19 @@ RSpec.describe "api/tests", type: :request do
     get "Retrieves a test" do
       tags "Tests"
       produces "application/json"
-
       parameter name: :uuid, in: :path, type: :string
 
       response "200", :success do
-        schema type: :object,
-               properties: {
-                 uuid: { type: :string },
-                 subject: { type: :string },
-                 question: { type: :string },
-                 answer: { type: :string },
-               },
-               required: %w(uuid subject question answer)
-
-        examples "application/json" => {
-          uuid: "b77eb326-efc1-41e1-a37e-c66bcff4f340",
-          subject: "Science",
-          question: "Is math related to science?",
-          answer: "Non't",
-        }
-
-        let(:uuid) { create(:test).id }
-
+        schema RswagHelper::RESOURCE_SCHEMA
+        examples RswagHelper::RESOURCE_EXAMPLE
+        let(:uuid) { create(:test, :binary_question).id }
         run_test!
       end
 
       response "404", :not_found do
-        schema type: :object,
-               properties: {
-                 detail: { type: :string },
-               }
-
-        examples "application/json" => {
-          detail: "Not found.",
-        }
-
+        schema RswagHelper::NOT_FOUND_SCHEMA
+        examples RswagHelper::NOT_FOUND_EXAMPLE
         let(:uuid) { "invalid" }
-
         run_test!
       end
     end
