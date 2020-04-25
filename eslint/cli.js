887b90a8636f85949a004fc6509733585d10b141
Jedrzej Ginter <jedrzej.ginter@monterail.com>
2020-04-25 20:37:10 +0200

    Use correct Node version, support local eslint config

--- eslint/cli.js
+++ eslint/cli.js
@@ -1,13 +1,18 @@
 #!/usr/bin/env node
 const cps = require("child_process");
+const fs = require("fs");
 const path = require("path");
 
-const argv = process.argv.slice(2);
-const eslintConfigPath = path.join(__dirname, "/eslint.config.js");
+const args = process.argv.slice(2);
+const eslintConfigPathInContext = path.join(process.cwd(), "eslint.config.js");
+
+const eslintConfigPath = fs.existsSync(eslintConfigPathInContext)
+  ? eslintConfigPathInContext
+  : path.join(__dirname, "eslint.config.js");
 
 try {
   cps.execSync(
-    `npx eslint --no-error-on-unmatched-pattern --config '${eslintConfigPath}' ${argv.join(" ")}`,
+    `npx eslint --no-error-on-unmatched-pattern --config '${eslintConfigPath}' ${args.join(" ")}`,
     {
       stdio: "inherit",
     },
