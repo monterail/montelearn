8160cafb6f5f74031d997d33f54e01856a07222a
Ewelina Buturla <ewelina.buturla@monterail.com>
2020-04-27 09:06:07 +0200

    Allow to make requests to tests-api

--- tests-api/config/application.rb
+++ tests-api/config/application.rb
@@ -35,7 +35,7 @@ module TestsApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-
+    config.hosts << "tests-api"
     config.active_record.schema_format = :sql
     config.generators.system_tests = nil
   end
