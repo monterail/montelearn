cddd09808ea35738baf88b15cdba4b9316c152e0
Aitor Lopez Beltran <aitorlopezbeltran@gmail.com>
2020-04-28 17:47:43 +0200

    [ME-23] Add ENV variable to dinamically whitelist CORS origins

--- tests-api/config/initializers/cors.rb
+++ tests-api/config/initializers/cors.rb
@@ -9,7 +9,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "*"
+    origins ENV.fetch("CORS_ORIGIN_WHITELIST") { "*" }
 
     resource "*",
              headers: :any,
