999c99d8c28baf10d4180dc593087578342a827e
Aitor Lopez Beltran <aitorlopezbeltran@gmail.com>
2020-04-30 09:30:06 +0200

    [ME-43] Update ApplicationController

--- tests-api/app/controllers/application_controller.rb
+++ tests-api/app/controllers/application_controller.rb
@@ -4,29 +4,43 @@ class ApplicationController < ActionController::API
   private
 
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+  rescue_from ActiveRecord::RecordInvalid, with: :render_validation_errors
+  rescue_from ActiveModel::UnknownAttributeError, with: :render_bad_request
 
-  def render_not_found
-    render json: { "detail": "Not found." }, status: :not_found
+  # Issue: https://github.com/rails/rails/issues/38285
+  # Workaround: https://github.com/rails/rails/issues/34244#issuecomment-433365579
+  def process_action(*args)
+    super
+  rescue ActionDispatch::Http::Parameters::ParseError => e
+    render_bad_request(e)
   end
 
-  def render_validation_errors(resource)
-    render json: resource.errors.messages, status: :bad_request
+  def render_not_found(exception)
+    render json: { detail: exception.message }, status: :not_found
+  end
+
+  def render_validation_errors(exception)
+    render json: exception.record.errors.messages, status: :bad_request
+  end
+
+  def render_bad_request(exception)
+    render json: { detail: exception.message }, status: :bad_request
   end
 
   def render_collection(relation)
     render json: {
-      "count": relation.size,
-      "next": nil,
-      "previous": nil,
-      "results": relation.map { |resource| presenter.new(resource).as_json },
+      count: relation.size,
+      next: nil,
+      previous: nil,
+      results: relation.map { |resource| present(resource) },
     }
   end
 
   def render_resource(resource, status = :ok)
-    render json: presenter.new(resource).as_json, status: status
+    render json: present(resource), status: status
   end
 
-  def presenter
-    "#{controller_path.classify}Presenter".constantize
+  def present(resource)
+    "#{resource.class}Presenter".constantize.new(resource).as_json
   end
 end
