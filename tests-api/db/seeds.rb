be8697057ef4b463a422f83a8f8b06b108a5c523
Aitor Lopez Beltran <aitorlopezbeltran@gmail.com>
2020-04-30 09:34:47 +0200

    [ME-43] Update seeds

--- tests-api/db/seeds.rb
+++ tests-api/db/seeds.rb
@@ -1,17 +1,28 @@
 # frozen_string_literal: true
 
-Test.create!(
-  subject: "Science",
-  question_type: "binary",
-  question: "Is math related to science?",
-  choices: [
-    {
-      answer: "Yesn't",
-      correct: false,
-    },
-    {
-      answer: "Non't",
-      correct: true,
-    },
-  ],
-)
+QUESTIONS_DATA = [
+  {
+    question_type: "binary",
+    content: "Is math related to science?",
+    choices: [
+      {
+        answer: "Yesn't",
+        correct: false,
+      },
+      {
+        answer: "Non't",
+        correct: true,
+      },
+    ],
+  },
+].freeze
+
+TESTS_DATA = [
+  {
+    lesson_uuid: "cf48df3b-ae86-464e-acd7-6cdca498fa8a",
+    title: "Test #1",
+    questions_attributes: QUESTIONS_DATA,
+  },
+].freeze
+
+TESTS = Test.create(TESTS_DATA)
