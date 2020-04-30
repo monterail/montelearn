ad81380f7456c7881c9338bbb98ebff6c7bc848e
Aitor Lopez Beltran <aitorlopezbeltran@gmail.com>
2020-04-30 16:15:14 +0200

    [ME-21] Remove comments

--- admin/src/dataProvider.js
+++ admin/src/dataProvider.js
@@ -36,82 +36,9 @@ const dataProvider = {
         id: json.uuid,
       }
     })),
-
-  // getMany: (resource, params) => {
-  //   const query = {
-  //       filter: JSON.stringify({ id: params.ids }),
-  //   };
-  //   const url = `${apiUrl}/${resource}?${JSON.stringify(query)}`;
-  //   return fetch(url).then(({ json }) => ({ 
-  //     data: json.map(resource => ({ ...resource, id: resource.uuid }) ),
-  //   }));
-  // },
-
-  // getManyReference: (resource, params) => {
-  //   const { page, perPage } = params.pagination;
-  //   const { field, order } = params.sort;
-  //   const query = {
-  //       sort: JSON.stringify([field, order]),
-  //       range: JSON.stringify([(page - 1) * perPage, page * perPage - 1]),
-  //       filter: JSON.stringify({
-  //           ...params.filter,
-  //           [params.target]: params.id,
-  //       }),
-  //   };
-  //   const url = `${apiUrl}/${resource}?${JSON.stringify(query)}`;
-
-  //   return fetch(url).then(({ headers, json }) => ({
-  //     data: json.map(resource => ({ ...resource, id: resource.uuid }) ),
-  //     total: parseInt(headers.get('content-range').split('/').pop(), 10),
-  //   }));
-  // },
-
-  // update: (resource, params) =>
-  //   fetch(`${apiUrl}/${resource}/${params.id}`, {
-  //       method: 'PUT',
-  //       body: JSON.stringify(params.data),
-  //   }).then(({ json }) => ({ 
-  //     ...json,
-  //     id: json.uuid,
-  //   })
-  // ),
-
-  // updateMany: (resource, params) => {
-  //   const query = {
-  //       filter: JSON.stringify({ id: params.ids}),
-  //   };
-  //   return fetch(`${apiUrl}/${resource}?${JSON.stringify(query)}`, {
-  //       method: 'PUT',
-  //       body: JSON.stringify(params.data),
-  //   }).then(({ json }) => ({ data: json }));
-  // },
-
-  // create: (resource, params) =>
-  //   fetch(`${apiUrl}/${resource}`, {
-  //       method: 'POST',
-  //       body: JSON.stringify(params.data),
-  //   }).then(({ json }) => ({
-  //     data: { ...params.data, id: json.uuid },
-  //   })),
-
-  // delete: (resource, params) =>
-  //   fetch(`${apiUrl}/${resource}/${params.id}`, {
-  //       method: 'DELETE',
-  //   }).then(({ json }) => ({ 
-  //     ...json,
-  //     id: json.uuid,
-  //   })
-  // ),
-
-  // deleteMany: (resource, params) => {
-  //   const query = {
-  //       filter: JSON.stringify({ id: params.ids}),
-  //   };
-  //   return fetch(`${apiUrl}/${resource}?${JSON.stringify(query)}`, {
-  //       method: 'DELETE',
-  //       body: JSON.stringify(params.data),
-  //   }).then(({ json }) => ({ data: json }));
-  // }
+  
+  // To add more methods see:
+  // https://marmelab.com/react-admin/DataProviders.html#writing-your-own-data-provider
 };
 
 export default dataProvider;
