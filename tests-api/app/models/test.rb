048f64fafeaef79fd68810d7635ccb29d265753d
Aitor Lopez Beltran <aitorlopezbeltran@gmail.com>
2020-05-04 15:32:38 +0200

    [ME-43] Update Test validations
    - uniqueness of lesson_uuid

--- tests-api/app/models/test.rb
+++ tests-api/app/models/test.rb
@@ -3,7 +3,8 @@
 class Test < ApplicationRecord
   has_many :questions, dependent: :destroy
 
-  validates :lesson_uuid, :title, presence: true
+  validates :lesson_uuid, presence: true, uniqueness: true
+  validates :title, presence: true
 
   accepts_nested_attributes_for :questions, allow_destroy: true, reject_if: :all_blank
 end
--- tests-api/spec/models/test_spec.rb
+++ tests-api/spec/models/test_spec.rb
@@ -3,18 +3,19 @@
 require "rails_helper"
 
 RSpec.describe Test, type: :model do
+  subject { build(:test) }
+
+  it { is_expected.to be_valid }
+
   describe "associations" do
     it { is_expected.to have_many(:questions).dependent(:destroy) }
   end
 
   describe "validations" do
     it { is_expected.to validate_presence_of(:lesson_uuid) }
+    it { is_expected.to validate_uniqueness_of(:lesson_uuid).case_insensitive }
     it { is_expected.to validate_presence_of(:title) }
   end
 
   it { is_expected.to accept_nested_attributes_for(:questions).allow_destroy(true) }
-
-  it "has a valid Factory" do
-    build(:test).should be_valid
-  end
 end
