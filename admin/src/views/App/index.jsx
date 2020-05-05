770e58c551d3b086848b2990dda497d7055d3ef3
Jedrzej Ginter <jedrzej.ginter@monterail.com>
2020-05-05 17:22:57 +0200

    [ME-21] Move services to separate module

--- admin/src/views/App/index.jsx
+++ admin/src/views/App/index.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Admin, Resource } from "react-admin";
 
+import authProvider from "@/services/auth-provider";
+import dataProvider from "@/services/data-provider";
+
 import LessonsList from "./components/LessonsList";
 import ShowLesson from "./components/ShowLesson";
-import authProvider from "./services/auth-provider";
-import dataProvider from "./services/data-provider";
 
 export default function App() {
   return (
