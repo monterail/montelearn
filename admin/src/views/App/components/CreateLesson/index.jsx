3bea60af7e3e8b101cea02346ff59582a8b80e7d
Aitor Lopez Beltran <aitorlopezbeltran@gmail.com>
2020-05-06 16:36:45 +0200

    [ME-63] Add CreateLesson component

---
+++ admin/src/views/App/components/CreateLesson/index.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import {
+  Create,
+  SimpleForm,
+  TextInput,
+  FileField,
+  FileInput,
+  required,
+  SelectInput,
+} from "react-admin";
+
+import { subjectChoices, gradeChoices } from "../../../../data/lessonChoices";
+
+export default function CreateLesson(props) {
+  return (
+    <Create {...props}>
+      <SimpleForm>
+        <TextInput source="name" validate={[required()]} />
+        <TextInput source="description" validate={[required()]} />
+        <FileInput
+          multiple={false}
+          source="pdf_file"
+          accept="application/pdf"
+          validate={[required()]}
+        >
+          <FileField source="src" title="title" />
+        </FileInput>
+        <TextInput source="url" />
+        <SelectInput source="subject" choices={subjectChoices} />
+        <SelectInput source="grade" choices={gradeChoices} />
+      </SimpleForm>
+    </Create>
+  );
+}
