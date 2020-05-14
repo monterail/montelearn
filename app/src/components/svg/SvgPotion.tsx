f128aa99ea4ff880ba404caa64906dc9ab7b5330
Maciej Hnat <39158460+mecies@users.noreply.github.com>
2020-05-14 08:34:16 +0200

    [ME-82] Choose Your Subject view implementation (#79)
    Implement the actual view
    
    Put all SVG components in one directory @/components/svg
    
    Updated imports for the ButtonWithArrow components to keep export default convention

---
+++ app/src/components/svg/SvgPotion.tsx
@@ -0,0 +1,122 @@
+import React from "react";
+
+const SvgPotion = (props: any) => {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M6.72595 0.75H17.226"
+        stroke="#F47073"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M15.726 8.25V0.75H8.22597V8.25L1.48897 18.615C1.19484 19.0675 1.02822 19.5909 1.00667 20.1302C0.985111 20.6694 1.10942 21.2045 1.36649 21.679C1.62356 22.1535 2.00387 22.5499 2.46734 22.8264C2.93082 23.1029 3.46029 23.2492 3.99997 23.25H19.948C20.488 23.2499 21.018 23.1041 21.482 22.8279C21.9461 22.5517 22.3269 22.1554 22.5845 21.6808C22.842 21.2061 22.9667 20.6708 22.9453 20.1312C22.9239 19.5916 22.7573 19.0678 22.463 18.615L15.726 8.25Z"
+        stroke="#F47073"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M5.30103 12.75H18.651"
+        stroke="#F47073"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M14.226 17.25H17.226"
+        stroke="#F47073"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M15.726 15.75V18.75"
+        stroke="#F47073"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M6.72595 19.125C6.82523 19.1253 6.92036 19.1649 6.99047 19.2352C7.06058 19.3055 7.09995 19.4007 7.09995 19.5"
+        stroke="#F47073"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M6.35095 19.5C6.35095 19.4005 6.39046 19.3052 6.46079 19.2348C6.53111 19.1645 6.6265 19.125 6.72595 19.125"
+        stroke="#F47073"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M6.72595 19.875C6.6265 19.875 6.53111 19.8355 6.46079 19.7652C6.39046 19.6948 6.35095 19.5995 6.35095 19.5"
+        stroke="#F47073"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M7.09998 19.5C7.09998 19.5995 7.06047 19.6948 6.99014 19.7652C6.91981 19.8355 6.82443 19.875 6.72498 19.875"
+        stroke="#F47073"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M9.72595 16.125C9.82541 16.125 9.92079 16.1645 9.99112 16.2348C10.0614 16.3052 10.101 16.4005 10.101 16.5"
+        stroke="#F47073"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M9.35095 16.5C9.35095 16.4005 9.39046 16.3052 9.46079 16.2348C9.53111 16.1645 9.6265 16.125 9.72595 16.125"
+        stroke="#F47073"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M9.72595 16.875C9.6265 16.875 9.53111 16.8355 9.46079 16.7652C9.39046 16.6948 9.35095 16.5995 9.35095 16.5"
+        stroke="#F47073"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M10.1 16.5C10.1 16.5995 10.0605 16.6948 9.99014 16.7652C9.91981 16.8355 9.82443 16.875 9.72498 16.875"
+        stroke="#F47073"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M15.726 3.75H12.726"
+        stroke="#F47073"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M15.726 6.75H12.726"
+        stroke="#F47073"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default SvgPotion;
