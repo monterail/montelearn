56bed19b09e37be0f1324b63ce9435ae7fb9e749
mecies <maciekhnat@gmail.com>
2020-05-19 09:45:43 +0200

    add fetching subject list
    refactor subject icoms to enum

--- app/src/utils/hooks/useRequest.ts
+++ app/src/utils/hooks/useRequest.ts
@@ -1,12 +1,42 @@
-import useSWR from "swr";
+import useSWR, { ConfigInterface, responseInterface } from "swr";
+import { AxiosRequestConfig, AxiosResponse, AxiosError } from "axios";
+
 import apiClient from "@/services/apiClient";
 
-export default function useRequest(request: any, { ...config } = {}) {
-  return useSWR(
+export type GetRequest = AxiosRequestConfig | null;
+
+interface Return<Data, Error>
+  extends Pick<
+    responseInterface<AxiosResponse<Data>, AxiosError<Error>>,
+    "isValidating" | "revalidate" | "error"
+  > {
+  data: Data | undefined;
+  response: AxiosResponse<Data> | undefined;
+}
+
+export default function useRequest<Data = unknown, Error = unknown>(
+  request: GetRequest,
+  { ...config }: ConfigInterface = {},
+): Return<Data, Error> {
+  const { data: response, error, isValidating, revalidate } = useSWR<
+    AxiosResponse<Data>,
+    AxiosError<Error>
+  >(
     request && JSON.stringify(request),
-    () => apiClient(request).then((response) => response.data),
+    // Typescript thinks `request` can be `null` here, but the fetcher
+    // * function is actually only called by `useSWR` when it isn't.
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    () => apiClient(request!),
     {
       ...config,
     },
   );
+
+  return {
+    data: response && response.data,
+    response,
+    error,
+    isValidating,
+    revalidate,
+  };
 }
