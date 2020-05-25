0d1e48716571a96f393db254e5719ab1e36bb007
ewelinabuturla <33395376+ewelinabuturla@users.noreply.github.com>
2020-05-25 10:41:49 +0200

    [ME-76] disable Django admin for lesson-api (#105)

--- lesson-api/api/lesson/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Grade, Lesson, Subject
-
-
-admin.site.register(Grade)
-admin.site.register(Lesson)
-admin.site.register(Subject)
--- lesson-api/config/urls.py
+++ lesson-api/config/urls.py
@@ -1,13 +1,11 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib import admin
 from django.urls import include, path, re_path
 
 from .schema import schema_view
 
 
 urlpatterns = [
-    path(settings.ADMIN_URL, admin.site.urls),
     path("api/", include("api.lesson.urls")),
 ]
 
