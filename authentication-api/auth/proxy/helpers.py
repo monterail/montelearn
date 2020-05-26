db030a9c57f06a46499fa22b94ca7da466f4a1e2
Ewelina Buturla <ewelina.buturla@monterail.com>
2020-05-26 09:58:21 +0200

    Fix

--- authentication-api/auth/proxy/helpers.py
+++ authentication-api/auth/proxy/helpers.py
@@ -86,11 +86,14 @@ class CustomProxyView(ProxyView):
                 )
             else:
                 headers = {"Content-Type": "application/json", "Accept": "application/json"}
+                if not isinstance(data, str):
+                    data = json.dumps(data)
+
                 response = requests.request(
                     request.method,
                     url,
                     params=params,
-                    data=json.dumps(data),
+                    data=data,
                     files=files,
                     headers=headers,
                     timeout=self.proxy_settings.TIMEOUT,
