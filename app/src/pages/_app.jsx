7c22cef56caafac677b4eee3da41eb50dba5ff7e
volodymyrhudyma <volodymyr.hudyma@monterail.com>
2020-05-26 14:19:03 +0200

    Add nprogress to app

--- app/src/pages/_app.jsx
+++ app/src/pages/_app.jsx
@@ -1,3 +1,8 @@
+import Router from "next/router";
+import NProgress from "nprogress";
+
+import "nprogress/nprogress.css";
+
 import "@/assets/css/tailwind.css";
 import "@/assets/css/global.css";
 
@@ -5,6 +10,10 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
 import { getAccessTokenOnServer } from "@/utils/helpers/auth";
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 export default function App({ Component, pageProps, isLoggedIn }) {
   return (
