d0f7a81a0f7a271930cd9ee6e8dcb4c652831303
Maciej Hnat <39158460+mecies@users.noreply.github.com>
2020-06-02 09:53:32 +0200

    [ME-145] Allow only binary questions in admin panel (#131)

--- admin/src/data/testChoices.js
+++ admin/src/data/testChoices.js
@@ -1,5 +1,4 @@
-export const questionTypeChoices = [
-  { id: "binary", name: "True/False" },
-  { id: "multiple-choice", name: "Multiple Choice" },
-  { id: "multiple-answer", name: "Multiple Answer" },
-];
+export const questionTypeChoices = [{ id: "binary", name: "True/False" }];
+// add to the object when multiple-choice and multiple-answer are supported
+// { id: "multiple-choice", name: "Multiple Choice" },
+// { id: "multiple-answer", name: "Multiple Answer" },
