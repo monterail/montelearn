d726099499522d90e99cd243004826e201947273
Maciej Hnat <39158460+mecies@users.noreply.github.com>
2020-06-02 12:04:30 +0200

    [ME-146] Display real pdf file name in lesson edit (#132)

--- admin/src/views/App/components/EditLesson/index.jsx
+++ admin/src/views/App/components/EditLesson/index.jsx
@@ -11,15 +11,22 @@ import {
 } from "react-admin";
 
 export default function EditLesson(props) {
-  const formatFile = (value) =>
-    typeof value === "string" ? { src: value, title: "Pdf file" } : value;
+  const formatFile = (value) => {
+    if (typeof value === "string") {
+      const pdfName = value.substring(value.indexOf("/lesson/") + 8);
+      // value is a url, after /lesson/ + 8 characters there will be a file name
+      return { src: value, title: pdfName };
+    }
+    return value;
+  };
+
   return (
     <Edit {...props}>
       <SimpleForm>
         <TextInput source="name" validate={[required()]} />
         <TextInput source="description" validate={[required()]} />
         <FileInput format={formatFile} multiple={false} source="pdf_file" accept="application/pdf">
-          <FileField source="src" title="title" />
+          <FileField source="src" title="title" target="_blank" />
         </FileInput>
         <TextInput source="url" />
         <ReferenceInput label="Subject" source="subject" reference="subject">
