52e0dfefd0c79d067fe9b6b1a162c57bc0d0a6bc
Maciej Hnat <39158460+mecies@users.noreply.github.com>
2020-06-03 11:42:40 +0200

    [ME-148] Remove unused landing package (#136)
    * remove landing package
    
    * update heroku oauth token

--- landing/src/pages/register.tsx
+++
@@ -1,5 +0,0 @@
-import Register from "@/components/Register";
-
-export default function RegisterPage() {
-  return <Register />;
-}
--- landing/src/views/Home/__tests__/index.spec.jsx
+++
@@ -1,9 +0,0 @@
-import { render } from "@testing-library/react";
-
-import Component from "..";
-
-describe("Home", () => {
-  it("should render without errors", () => {
-    expect(() => render(<Component />)).not.toThrow();
-  });
-});
