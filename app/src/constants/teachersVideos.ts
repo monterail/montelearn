5fc8d41b1449c4e3487afadb86d7fe447a44b625
Maciej Hnat <39158460+mecies@users.noreply.github.com>
2020-06-09 11:45:37 +0200

    [ME-170] Improve embedded videos in teachers page #146

--- app/src/constants/teachersVideos.ts
+++ app/src/constants/teachersVideos.ts
@@ -7,32 +7,32 @@ interface VideoOption extends DropdownOption {
 export const teachersVideos: VideoOption[] = [
   {
     value: "createLesson",
-    name: "Create lesson (to be recorded)",
-    url: "https://drive.google.com/file/d/1MMnEJEsVcTIbQKTT3UVk5UX4pyNdV6br/preview", // todo
+    name: "Create lesson (to be re-recorded)",
+    url: "https://drive.google.com/file/d/1MMnEJEsVcTIbQKTT3UVk5UX4pyNdV6br/preview", // to be recorded
   },
   {
     value: "createTest",
     name: "Create test",
-    url: "https://drive.google.com/file/d/1mEpN_ADJ_VgeeIEAVoU7BM7z9iFZeaHE/preview",
+    url: "https://www.youtube.com/embed/fUTnB9jwykU",
   },
   {
     value: "editLesson",
     name: "Edit lesson",
-    url: "https://drive.google.com/file/d/1XZuUwt1ii6bCO7UUDqs2A78yNaCrc465/preview",
+    url: "https://www.youtube.com/embed/NCsXABLG8C4",
   },
   {
     value: "editTest",
     name: "Edit test",
-    url: "https://drive.google.com/file/d/1aEUwe5-ZvZ0_XMQVADsKwkSwscndMkWJ/preview",
+    url: "https://www.youtube.com/embed/QjnoaI8Ej_I",
   },
   {
     value: "deleteLesson",
     name: "Delete lesson",
-    url: "https://drive.google.com/file/d/1yhM9eQ8_9a_Jhm4MCIbTQVCydMZR-rch/preview",
+    url: "https://www.youtube.com/embed/lSBE6jruCzU",
   },
   {
     value: "deleteTest",
     name: "Delete test",
-    url: "https://drive.google.com/file/d/14bxSuBFIMYZ3EqHGWirdC_Cixb4c5ELd/preview",
+    url: "https://www.youtube.com/embed/fUTnB9jwykU",
   },
 ];
