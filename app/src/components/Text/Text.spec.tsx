b129d37c45b1e7d490e1a314900ad068b36635e6
Maciej Hnat <39158460+mecies@users.noreply.github.com>
2020-06-10 14:29:28 +0200

    [ME-172] Unit tests for Title, SubTitle and Text components (#149)

---
+++ app/src/components/Text/Text.spec.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { shallow } from "enzyme";
+
+import Text from "./Text";
+
+describe("Text", () => {
+  describe("className", () => {
+    const customClass = "custom-class";
+    it("should render Text without custom class", () => {
+      const wrapper = shallow(<Text />);
+      expect(wrapper.find("p").hasClass(customClass)).toBe(false);
+    });
+    it("should render Text with custom class", () => {
+      const wrapper = shallow(<Text className={customClass} />);
+      expect(wrapper.find("p").hasClass(customClass)).toBe(true);
+    });
+  });
+
+  describe("children", () => {
+    const customText = "Custom text";
+    it("should render Text with custom text", () => {
+      const wrapper = shallow(<Text>{customText}</Text>);
+      expect(wrapper.find("p").text()).toMatch(customText);
+    });
+    it("should render Text with custom child paragraph", () => {
+      const customParagraph = `<span>${customText}</span>`;
+      const wrapper = shallow(
+        <Text>
+          <span>{customText}</span>
+        </Text>,
+      );
+      expect(wrapper.find("p").find("span").html()).toEqual(customParagraph);
+    });
+  });
+});
---
+++ app/src/components/Title/Title.spec.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { shallow } from "enzyme";
+
+import Title from "./Title";
+
+describe("Title", () => {
+  describe("className", () => {
+    const customClass = "custom-class";
+    it("should render Title without custom class", () => {
+      const wrapper = shallow(<Title />);
+      expect(wrapper.find("h1").hasClass(customClass)).toBe(false);
+    });
+    it("should render Title with custom class", () => {
+      const wrapper = shallow(<Title className={customClass} />);
+      expect(wrapper.find("h1").hasClass(customClass)).toBe(true);
+    });
+  });
+
+  describe("children", () => {
+    const customText = "Custom text";
+    it("should render Title with custom text", () => {
+      const wrapper = shallow(<Title>{customText}</Title>);
+      expect(wrapper.find("h1").text()).toMatch(customText);
+    });
+    it("should render Title with custom child paragraph", () => {
+      const customParagraph = `<p>${customText}</p>`;
+      const wrapper = shallow(
+        <Title>
+          <p>{customText}</p>
+        </Title>,
+      );
+      expect(wrapper.find("h1").find("p").html()).toEqual(customParagraph);
+    });
+  });
+});
