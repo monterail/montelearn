40ddaa845c244de320912c5a6335892726a70964
Bartosz Gamza <bartosz.gamza@monterail.com>
2020-06-15 13:52:38 +0200

    add breadcrumb tests

---
+++ app/src/components/Breadcrumbs/Breadcrumbs.spec.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { render, cleanup, fireEvent } from "@testing-library/react";
+
+import Breadcrumbs from "./Breadcrumbs";
+
+describe("Breadcrumbs", () => {
+  afterEach(() => cleanup);
+
+  it("renders correct options", async () => {
+    const { findAllByTestId } = render(<Breadcrumbs options={["David", "Bowie"]} />);
+    expect(await findAllByTestId("element")).toHaveLength(2);
+  });
+
+  it("calls backClick function", () => {
+    const backClickMock = jest.fn();
+    const { getByRole } = render(<Breadcrumbs options={["One"]} handleBackClick={backClickMock} />);
+    fireEvent.click(getByRole("button"));
+    expect(backClickMock).toHaveBeenCalled();
+  });
+});
--- app/src/components/Breadcrumbs/Breadcrumbs.tsx
+++ app/src/components/Breadcrumbs/Breadcrumbs.tsx
@@ -20,7 +20,11 @@ const Breadcrumbs: FunctionComponent<Props> = ({ options, back, handleBackClick 
       const isLast = isLastElement(index, options.length);
 
       return (
-        <li key={index} className={`capitalize text-sm sm:text-base ${isLast ? "text-black" : ""}`}>
+        <li
+          key={index}
+          className={`capitalize text-sm sm:text-base ${isLast ? "text-black" : ""}`}
+          data-testid="element"
+        >
           {el}
           {!isLast && <span className="px-1 sm:px-4">/</span>}
         </li>
@@ -31,6 +35,7 @@ const Breadcrumbs: FunctionComponent<Props> = ({ options, back, handleBackClick 
     <div className="sm:p-6 bg-red-100 flex flex-row font-roboto-mono">
       {back && (
         <ButtonWithArrow
+          data-testid="arrow-btn"
           onClick={handleBackClick}
           direction={BUTTON_DIRECTIONS.LEFT}
           withBorder={false}
