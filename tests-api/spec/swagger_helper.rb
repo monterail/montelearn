e921d9d056b2986953e536f8fc7562827b2fb84a
Piotr Skoczylas <piotr.skoczylas@monterail.com>
2020-06-16 17:43:46 +0200

    [ME-129] Change in swagger helper

--- tests-api/spec/swagger_helper.rb
+++ tests-api/spec/swagger_helper.rb
@@ -16,7 +16,7 @@ RSpec.configure do |config|
   # the root example_group in your specs, e.g. describe '...', swagger_doc: 'v2/swagger.json'
   config.swagger_docs = {
     "v1/swagger.yaml" => {
-      openapi: "3.0.1",
+      swagger: "2.0",
       info: {
         title: "API V1",
         version: "v1",
