fb3be6b02343c76de84458e422025132fcd34e38
Oleś Kulczewicz <15707597+kulczewicz@users.noreply.github.com>
2020-06-23 17:39:34 +0200

    [ME-173] Add tests for Button and ButtonWithArrow (#151)
    * [ME-173] Add tests for Button and ButtonWithArrow
    
    * [ME-173] Remove snapshots
    
    * [ME-173] Migrate enzyme to testing-library
    
    * [ME-173] Remove asyncs
    
    * [ME-173] Remove unnecessary imports

---
+++ app/src/components/Button/Button.spec.tsx
@@ -0,0 +1,32 @@
+import { render, cleanup, fireEvent } from "@testing-library/react";
+
+import Button from "./Button";
+
+describe("Button", () => {
+  afterEach(() => cleanup);
+
+  describe("className", () => {
+    const customClass = "custom-class";
+    it("should render Title with custom class", () => {
+      const { getByRole } = render(<Button className={customClass} />);
+      expect(getByRole("button")).toHaveClass(customClass);
+    });
+  });
+
+  describe("onClick", () => {
+    const mockOnClick = jest.fn();
+    it("should call mock function when button is clicked", () => {
+      const { getByRole } = render(<Button onClick={mockOnClick} />);
+      fireEvent.click(getByRole("button"));
+      expect(mockOnClick).toHaveBeenCalled();
+    });
+  });
+
+  describe("children", () => {
+    const buttonText = "Button text";
+    it("should render Button with custom text", () => {
+      const { getByRole } = render(<Button>{buttonText}</Button>);
+      expect(getByRole("button")).toHaveTextContent(buttonText);
+    });
+  });
+});
