c94a9081f4d4766152f091245fc4061adb3acde4
mecies <maciekhnat@gmail.com>
2020-06-23 12:14:39 +0200

    add new content

--- app/src/constants/content.ts
+++ app/src/constants/content.ts
@@ -3,15 +3,15 @@ import { teachersVideos } from "./teachersVideos";
 export const landingContent = {
   title: "Build your own learning experience",
   subtitle:
-    "We provide a rich API for developers who can build custom learning software for students on every education level",
+    "Montelearn is a backend API that developers can use to build a custom e-learning platform. Its main focus is on creating interactive lessons and tests, suitable for every education level, science, or topic.",
   students: {
     name: "Students",
-    content: "Discover what the lessons look like in our application",
+    content: "Discover what lessons look like in our application.",
   },
   teachers: {
     name: "Teachers",
     content:
-      "Check how you can easily share materials with your students and add tests that check knowledge after the lesson",
+      "See how you can share lessons with your students and create tests that check their knowledge.",
   },
   developers: {
     name: "API Developers",
@@ -23,13 +23,13 @@ export const landingContent = {
 export const developersContent = {
   title: "Select example endpoint",
   subtitle:
-    "Take a look on example endpoints that our backend provides.The responses are mocked, to get more features set the app locally.",
+    "Take a look at example endpoints that our backend provides. The responses are mocked, to get more features set the app locally.",
 };
 
 export const teachersContent = {
   title: "Instructional videos",
   subtitle:
-    "To quickly understand how teachers' admin panel works, watch the instructional videos listed below",
+    "To quickly understand how teachers' admin panel works, watch the instructional videos listed below.",
   content: "More content",
   defaultVideo: teachersVideos[0],
 };
