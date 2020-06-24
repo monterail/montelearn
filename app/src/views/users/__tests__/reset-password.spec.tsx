f82a5ffb14dc049ad17f1d601cb01ae369c954c8
mecies <maciekhnat@gmail.com>
2020-06-24 10:53:15 +0200

    tests wip

--- app/src/views/users/__tests__/reset-password.spec.tsx
+++ app/src/views/users/__tests__/reset-password.spec.tsx
@@ -1,10 +1,28 @@
-import { render } from "@testing-library/react";
+import { render, cleanup } from "@testing-library/react";
 
 import ResetPassword from "../reset-password";
 
 describe("ResetPassword", () => {
-  it("renders reset password page correctly", async () => {
-    const { findByText } = render(<ResetPassword uid="fake_id" token="fake_token" />);
-    await findByText("Submit");
+  afterEach(cleanup);
+  const testId = "fake_id";
+  const testToken = "fake_token";
+
+  describe("props", () => {
+    it("renders props co=ause why nt", () => {
+      render(<ResetPassword uid={testId} token={testToken} />);
+    });
+  });
+  describe("behaviour", () => {
+    it("renders password reset form form", async () => {
+      const { findByText, findByLabelText } = render(
+        <ResetPassword uid={testId} token={testToken} />,
+      );
+      const buttonElement = await findByText(/submit/i);
+
+      expect(await findByLabelText(/new password/i)).toBeInTheDocument();
+      expect(await findByLabelText(/confirm password/i)).toBeInTheDocument();
+      expect(buttonElement).toBeInTheDocument();
+      expect(buttonElement).toHaveTextContent(/submit/i);
+    });
   });
 });
